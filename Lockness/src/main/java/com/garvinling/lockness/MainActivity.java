d19616dff0957266bb548218752727b62f70369c
Garvin <garvin.ling@student.csulb.edu>
2013-12-30 23:35:09 -0800

    Added custom font / Bluetooth Connection
    Included external font file and established bluetooth connection to
    hardware device.

--- Lockness/src/main/java/com/garvinling/lockness/MainActivity.java
+++ Lockness/src/main/java/com/garvinling/lockness/MainActivity.java
@@ -1,37 +1,31 @@
 package com.garvinling.lockness;
 
-import android.app.Activity;
 ;
 import android.app.ActionBar;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.media.audiofx.BassBoost;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.TextView;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 
 import com.garvinling.bluetooth.BTAdapter;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends FragmentActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -40,7 +34,7 @@ public class MainActivity extends FragmentActivity
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
      */
     private NavigationDrawerFragment mNavigationDrawerFragment;
-    private LockControlFragment lock_fragment = new LockControlFragment();
+    private LocknessFinderFragment lock_fragment = new LocknessFinderFragment();
 
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
@@ -92,7 +86,7 @@ public class MainActivity extends FragmentActivity
 
         super.onStart();
 
-
+        lock_fragment.setTypeFace();
         init();
 
         if(btAdapter == null)
@@ -110,10 +104,17 @@ public class MainActivity extends FragmentActivity
             }
 
             getPairedDevices();             //Retrieve list of paired devices and store it in an ArrayList::pairedDevices
-            startDiscovery();               //Retrieve list of discovered bluetooth devices and store it in an ArrayList::discoveredDevices
 
-            printPairedDevices();
-            printDiscoveredDevices();
+
+            if(LOCKNESS_DEVICE_EXISTS == 0)
+            {
+                   startDiscovery();        //Retrieve list of discovered bluetooth devices and store it in an ArrayList::discoveredDevices
+            }
+            else
+            {                               //Connect to the paired device.
+                   connectToPairedLocknessDevice();
+
+            }
 
         }
 
@@ -140,6 +141,8 @@ public class MainActivity extends FragmentActivity
 
     private void getPairedDevices(){
 
+            //Check the list of paired devices and see if one of them is a lockness device.
+
 
             devicesArray = btAdapter.getBondedDevices();
             if(devicesArray.size() > 0 )
@@ -148,13 +151,17 @@ public class MainActivity extends FragmentActivity
 
                             String listString = device.getName() + "  ::  " + device.getAddress();
                             pairedDevices.add(listString);
-                            print("Paired Device: " +listString);
+                            checkForPairedLocknessDevice(listString);
+
+                            if(LOCKNESS_DEVICE_EXISTS == 1){                //Connect to the first device that is a lockness device
+                                locknessDevice = device;                    //Set the bluetoothDevice to the matched lockness device
+                                break;
+                            }
+
                    }
 
 
             }
-
-                //TODO: change name of BT device and implement listview for click to connect.
     }
 
 
@@ -184,32 +191,38 @@ public class MainActivity extends FragmentActivity
                 public void onReceive(Context context, Intent intent) {
 
                         String action = intent.getAction();
+
+
                         if(BluetoothDevice.ACTION_FOUND.equals(action)){
 
+                            print("Discovered Device.  Adding to Devices ArrayList.");
                             BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                            boolean valid_device = checkValidLocknessDevice(device.getName());
+                            String discovered = device.getName() +" : " +device.getAddress();
+
                             devices.add(device);
                             String s = "";
 
-                            for(int a = 0; a < pairedDevices.size();a++){
+                            for(int a = 0; a < pairedDevices.size(); a++){
 
-                                if(device.getName().equals(pairedDevices.get(a))){
+                                if(LOCKNESS_DEVICE_EXISTS == 1){              //Checks if lockness is one of the paired devices on the phone/tablet
 
-                                    s = "(PAIRED)";
-
+                                    //connectToExistingLockness(device);        //Connect to the paired device
                                 }
 
                             }
 
-                            String discovered = device.getName() + " " +s+ " " +device.getAddress();
                             discoveredDevices.add(discovered);
 
-                            if(discovered.contains("SGH"))
+                            if(discovered.contains("RN42-0576"))
                             {
-                                lock_fragment.setText("Lockness device found!");
+                                lock_fragment.setText("Please Pair your Lockness!");
+                                LOCKNESS_DEVICE_ONLINE = 1;
                             }
 
 
                             print("Discovered Device added: " +device.getName() + " :: " +device.getAddress());
+
                         }
                         else if(BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action))
                         {
@@ -264,6 +277,10 @@ public class MainActivity extends FragmentActivity
 
             current_position = position;
             Bundle args = new Bundle();
+
+            //Update UI font elements
+            lock_fragment.setTypeFace();
+
             //args.putInt("test",position);
             //com.lock_fragment.setArguments(args);
            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,lock_fragment,"lock").setTransition(android.support.v4.app.FragmentTransaction.TRANSIT_FRAGMENT_OPEN).addToBackStack("lock").commit();
@@ -287,9 +304,6 @@ public class MainActivity extends FragmentActivity
 
             current_position = position;
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,settings_fragment,"settings").setTransition(android.support.v4.app.FragmentTransaction.TRANSIT_FRAGMENT_OPEN).addToBackStack("settings").commit();
-
-
-
 
         }
 
@@ -396,13 +410,185 @@ public class MainActivity extends FragmentActivity
 
 
 
+    private void checkForPairedLocknessDevice(String deviceString){
+
+            if(deviceString.contains("RN42")){
+
+                print("Lockness Device exists on this phone");
+                LOCKNESS_DEVICE_EXISTS = 1;
+
+            }
+
+
+    }
+
+
+
+
+    private boolean checkValidLocknessDevice(String deviceString){
+
+            if(deviceString.contains("RN42")){
+
+                return true;
+            }
+
+        return false;
+    }
+
+    private void connectToPairedLocknessDevice(){
+
+        print("Connecting to Lockness");
+        ConnectThread bt_connect = new ConnectThread(locknessDevice);           //Connect to lockness device
+        bt_connect.run();
+
+
+    }
+
 
 
     /** Bluetooth Connect Thread Functions **/
 
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket,
+            // because mmSocket is final
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
+            try {
+                // MY_UUID is the app's UUID string, also used by the server code
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) { System.out.println("IOEXCEPTION"); }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it will slow down the connection
+            btAdapter.cancelDiscovery();
+
+            try {
+                // Connect the device through the socket. This will block
+                // until it succeeds or throws an exception
+                mmSocket.connect();
+
+
+            } catch (IOException connectException) {
+
+                print("Unable to connect");
+                lock_fragment.setText("Unable to Connect to Lockness.)");
+
+                try {
+
+                    mmSocket.close();
+
+                } catch (IOException closeException) { }
 
 
 
+                return;
+            }
+
+            mHandler.obtainMessage(SUCCESS_CONNECT, mmSocket).sendToTarget();
+
+        }
+
+        /** Will cancel an in-progress connection, and close the socket */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
+    }
+
+
+
+    /** Bluetooth Connected Thread Functions **/
+
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer;  // buffer store for the stream
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    buffer = new byte[1024];
+                    bytes = mmInStream.read(buffer);
+                    // Send the obtained bytes to the UI activity
+
+                    //mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer).sendToTarget();
+
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) { }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
+    }
+
+    Handler mHandler = new Handler() {
+
+
+        @Override
+        public void handleMessage(Message msg) {
+            // TODO Auto-generated method stub
+            super.handleMessage(msg);
+            switch(msg.what){
+                case SUCCESS_CONNECT:
+                    // DO something
+                    print("Success_Connect");
+                    ConnectedThread connectedThread = new ConnectedThread((BluetoothSocket)msg.obj);
+                    Toast.makeText(getApplicationContext(), "CONNECT", 0).show();
+                    String s = "l";
+                    connectedThread.write(s.getBytes());
+                    break;
+                case MESSAGE_READ:
+                    byte[] readBuf = (byte[])msg.obj;
+                    String string = new String(readBuf);
+                    Toast.makeText(getApplicationContext(), string, 0).show();
+                    break;
+            }
+        }
+    };
 
 
 
@@ -410,13 +596,24 @@ public class MainActivity extends FragmentActivity
     private SettingsFragment settings_fragment = new SettingsFragment();
     private Bundle instance_state;
     private int current_position = 0;
+    protected static final int SUCCESS_CONNECT = 0;
+    protected static final int MESSAGE_READ = 1;
+    private int LOCKNESS_DEVICE_EXISTS = 0;
+    private int LOCKNESS_DEVICE_ONLINE = 0;
+    private String device_identifier;      //need to pull device string from sql.
+
+
+    BluetoothDevice locknessDevice;
     BluetoothAdapter btAdapter;
+    BluetoothSocket btSocket;
     Set<BluetoothDevice> devicesArray;
     ArrayList<BluetoothDevice> devices;
     ArrayList<String> pairedDevices;
     ArrayList<String> discoveredDevices;
     IntentFilter filter;
     BroadcastReceiver receiver;
+
+    public static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
 
 }
